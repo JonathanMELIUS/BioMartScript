88fae26f318af7a93599b1f574653b95348c71b5
Egon Willighagen <egon.willighagen@gmail.com>
2019-03-14 11:07:30 +0100

    Missing import

--- src/main/java/script/Main.java
+++ src/main/java/script/Main.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.Date;
 import java.util.HashMap;
